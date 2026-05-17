afd7102e3823e6eadf90dd3de81592b7a88547b5
Lucas Figueroa <lukafigueroa64@gmail.com>
2026-05-17 04:15:52 -0300

    FixDeploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto es importante para Netlify
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  publicDir: 'public', // Asegura que la carpeta public se copie
 })
