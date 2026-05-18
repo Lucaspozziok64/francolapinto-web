9eacac1ce631482917a5cf7ef604e617899104a4
Lucas Figueroa <lukafigueroa64@gmail.com>
2026-05-18 15:56:39 -0300

    Agregue cache buster

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,32 @@ import PartnerSponsors from "./pages/PartnerSponsors";
 import History from "./pages/History";
 import ScrollToTop from "./components/ScrollToTop";
 import InstallPrompt from "./components/InstallPrompt";
+import { CacheBuster } from "@piplup/cache-buster";
 
 function App() {
   return (
     <>
-      <InstallPrompt />
-      <BrowserRouter>
-        <Navbar />
-        <main>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/f1-2025" element={<F1Results />} />
-            <Route path="/f1-2026" element={<F1Results2026 />} />
-            <Route path="/partner-sponsor" element={<PartnerSponsors />} />
-            <Route path="/history" element={<History />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <CacheBuster
+        loading={
+          <div className="loader-wrap">Verificando actualizaciones...</div>
+        }
+        enabled={import.meta.env.PROD} // Solo funciona en producción
+      >
+        <InstallPrompt />
+        <BrowserRouter>
+          <Navbar />
+          <main>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/f1-2025" element={<F1Results />} />
+              <Route path="/f1-2026" element={<F1Results2026 />} />
+              <Route path="/partner-sponsor" element={<PartnerSponsors />} />
+              <Route path="/history" element={<History />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </CacheBuster>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto es importante para Netlify
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
+  base: "./", // Esto es importante para Netlify
+  define: {
+    // Esto crea un número único cada vez que construyes (build)
+    "import.meta.env.VITE_RELEASE_VERSION": JSON.stringify(Date.now()),
   },
-  publicDir: 'public', // Asegura que la carpeta public se copie
-})
+});
